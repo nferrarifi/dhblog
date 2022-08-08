c9df2400e7d37d07d9346db4b765ddb4cd471e31
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-08-08 17:10:34 -0300

    Fixing env

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
-  const movies = await (await fetch("http://localhost:3001/api/movies")).json();
-  console.log(movies);
-  res.status(200).json(movies);
-}
