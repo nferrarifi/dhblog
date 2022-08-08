0318d576608ce313412914f061848c9d24df5190
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-08-08 17:14:30 -0300

    Env fix

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const config = {
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
-    : "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000",
 };
 export default config;
