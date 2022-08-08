040b77c1952e00048d2325f75536bd6e6ac41d55
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-08-08 17:25:03 -0300

    fixing config URL to incluce http:// before domain vercel URL

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const config = {
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000",
 };
 export default config;
